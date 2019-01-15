b92f6274d265ac9f7d0a2e65f0f38a3256999e5e
Debayan Singha <debayansingha@gmail.com>
2019-01-15 21:55:57 +0530

    committed build files

---
+++ build/precache-manifest.b506c5f20487c7be26de9c996c3ef0c7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c23d20f0c4ae7e95616b25ec5f4a1b2",
+    "url": "/static/media/user.3c23d20f.png"
+  },
+  {
+    "revision": "c7806b0fda100000af1158691550c9da",
+    "url": "/static/media/shape.c7806b0f.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ee4d9b62c4985b09a375",
+    "url": "/static/js/main.ee4d9b62.chunk.js"
+  },
+  {
+    "revision": "7f267f7c200bce042178",
+    "url": "/static/js/1.7f267f7c.chunk.js"
+  },
+  {
+    "revision": "ee4d9b62c4985b09a375",
+    "url": "/static/css/main.149a6d1c.chunk.css"
+  },
+  {
+    "revision": "7f267f7c200bce042178",
+    "url": "/static/css/1.babe8999.chunk.css"
+  },
+  {
+    "revision": "d13f8cc81a55ea4c01b2e34fb68c08ec",
+    "url": "/index.html"
+  }
+];
