069ca33ae57e6ce61f2c72f12aad5b2d6531aef0
Debayan Singha <debayansingha@gmail.com>
2019-01-15 21:55:31 +0530

    added server

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const path = require('path')
+
+const app = express();
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, './build')))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log(`listening on ${port}`);
